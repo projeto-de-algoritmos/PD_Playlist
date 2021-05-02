40f91498424093a6905fe8f755e0e06126199c48
Wagner Martins <wagnermc506@gmail.com>
2021-05-01 23:43:53 -0300

    Atualizando app

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {getAllVideos} from './structures/channel';
+import getData from './structures/data';
+import Playlist from './structures/playlist';
+
+import React, {useState} from 'react';
+
 function App() {
+  
+  const [channels, setChannels] = useState(getData());
+  const [disponibleTime, setDisponibleTime] = useState(0);
+
+  const up = (i) => {
+    let channels_cp = [...channels];
+    channels_cp[i].priority += 1;
+    setChannels([...channels_cp]);
+  }
+
+  const down = (i) => {
+    let channels_cp = [...channels];
+    if (channels_cp[i].priority > 0) {
+      channels_cp[i].priority -= 1;
+    }
+    setChannels([...channels_cp]);
+  }
+
+  const main = () => {
+    let channels_cp = [...channels];
+    for (let channel of channels_cp) {
+      channel.propagate()
+    }
+    let videos = getAllVideos(channels_cp);
+    let playlist = new Playlist(videos);
+    console.log(playlist.getOPT(disponibleTime));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={main}>GO</button>
+      <input type="number" onChange={(e) => {setDisponibleTime(e.target.valueAsNumber)}}></input>
+      {channels.map((canal, i) => (
+        <div key={i}>
+          <button onClick={(e) => up(i)}>aumentar</button>
+          <p>{canal.name} - {canal.priority}</p>
+          <button onClick={(e) => down(i)}>diminuir</button>
+        </div>
+      ))}
     </div>
   );
 }
