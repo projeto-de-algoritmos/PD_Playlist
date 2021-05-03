3ad5fbb0f707c4df994d20e19a27ded9d3925230
Antonio Ruan Moura Barreto <ruanmoura13@outlook.com>
2021-05-03 03:52:43 -0300

    Adicionado telas para visualisar canais e modificar sua prioridade

--- app/src/App.js
+++ app/src/App.js
@@ -8,22 +8,22 @@ import React, {useState} from 'react';
 
 function App() {
   
-  const [channels, setChannels] = useState(getData());
+  const [channels, setChannels] = useState(getData);
   const [disponibleTime, setDisponibleTime] = useState(0);
 
-  const up = (i) => {
-    let channels_cp = [...channels];
-    channels_cp[i].priority += 1;
-    setChannels([...channels_cp]);
-  }
+  // const up = (i) => {
+  //   let channels_cp = [...channels];
+  //   channels_cp[i].priority += 1;
+  //   setChannels([...channels_cp]);
+  // }
 
-  const down = (i) => {
-    let channels_cp = [...channels];
-    if (channels_cp[i].priority > 0) {
-      channels_cp[i].priority -= 1;
-    }
-    setChannels([...channels_cp]);
-  }
+  // const down = (i) => {
+  //   let channels_cp = [...channels];
+  //   if (channels_cp[i].priority > 0) {
+  //     channels_cp[i].priority -= 1;
+  //   }
+  //   setChannels([...channels_cp]);
+  // }
 
   const main = () => {
     let channels_cp = [...channels];
@@ -35,14 +35,40 @@ function App() {
     console.log(playlist.getOPT(disponibleTime));
   }
 
+  const Videos = (props) => {
+    let vids = props.videos;
+    let saida = [];
+    for(let j = 0; j < vids.length; j++){
+      saida.push(
+        <tr>
+          <td className="video-el">{vids[j].name}</td>
+          <td className="video-el">{vids[j].time}</td>
+        </tr>
+      )
+    }
+
+    return saida
+  }
+
   const Channel = () => {
     let res = [];
     for(let i =0; i < 5; i++){
       res.push(
         <div className="channel">
-          <text>Nome do canal</text>
-          <div>set prioridade</div>
-          <div>lista de videos</div>
+          <text>{channels[i].name}</text>
+          <div>
+            <text>Prioridade do canal  </text>
+            <input className="channelPriority" type="number" onChange={(e) => {channels[i].priority = e.target.valueAsNumber}} defaultValue={channels[i].priority}></input>
+          </div>
+          <div className="tabela">
+            <table>
+              <tr>
+                <th className="video-el">Vídeo</th>
+                <th className="video-el">Tempo</th>
+              </tr>
+              <Videos videos={channels[i].videos}></Videos>
+            </table>
+          </div>
         </div>
       )
     }
